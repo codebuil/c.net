764d6ad385f88719897e6404fd780a463b0ace73
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-01 18:37:01 +0100

    mult 10 i64

---
+++ mult10.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class HelloWorld
+{
+    static public void Main ()
+    {
+        int n;
+        long l=1;
+        Console.WriteLine ("\x1bc\x1b[42;30m ]Hello World");
+        
+           for (n=0;n<19;n++){
+               Console.WriteLine("d{0}",l);
+               Console.WriteLine("h{0:x}",l);
+               l=mults(l,10);
+           }
+    }
+    static public long mults(long a,long b){
+        return a*b;
+    }
+
+}
